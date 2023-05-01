73a60084a62b6729ddd12d4f2dd03a01cf02fa82
firgiandhito <76060555+firgiandhito@users.noreply.github.com>
2023-05-01 17:02:16 +0700

    fourth

--- js/script.js
+++ js/script.js
@@ -6,12 +6,31 @@ function whatsapp() {
   var phone = document.getElementById("phone").value;
   var father = document.getElementById("father").value;
   var parentphone = document.getElementById("parent-phone").value;
-  var message = document.getElementById("message").value;
+  var education = document.getElementById("education").value;
+  var pastschool = document.getElementById("past-school").value;
+  var arabic = document.getElementById("arabic-value").value;
+  var english = document.getElementById("english-value").value;
+  var math = document.getElementById("math-value").value;
+  var it = document.getElementById("it-value").value;
+  var recitation = document.getElementById("recitation").value;
+  var achievement = document.getElementById("achievement").value;
+  var superiority = document.getElementById("superiority").value;
+  var hobby = document.getElementById("hobby").value;
+  var blog = document.getElementById("blog").value;
+  var video = document.getElementById("video").value;
+  var facebook = document.getElementById("facebook").value;
+  var instagram = document.getElementById("instagram").value;
+  var whatsapp = document.getElementById("whatsapp").value;
+  var example = document.getElementById("example").value;
+  var recommendation = document.getElementById("recommendation").value;
+  var motivation = document.getElementById("motivation").value;
+  var readiness = document.getElementById("readiness").value;
 
   var url =
     "https://wa.me/6285342894681?text=" +
     "*Calon Santri Baru Ma'had IT* " +
     "%0a" +
+    "*DATA PRIBADI*" +
     "%0a" +
     "*Nama :* " +
     name +
@@ -34,8 +53,79 @@ function whatsapp() {
     "*No. telepon ortu :* " +
     parentphone +
     "%0a" +
-    "*Pesan :* " +
-    message;
+    "%0a" +
+    "*RIWAYAT PENDIDIKAN*" +
+    "%0a" +
+    "*Pendidikan Terakhir :* " +
+    education +
+    "%0a" +
+    "*Alamat Sekolah Terakhir :* " +
+    pastschool +
+    "%0a" +
+    "*Nilai B. Arab :* " +
+    arabic +
+    "%0a" +
+    "*Nilai B. Inggris :* " +
+    english +
+    "%0a" +
+    "*Nilai Matematika :* " +
+    math +
+    "%0a" +
+    "*Nilai TIK :* " +
+    it +
+    "%0a" +
+    "*Jumlah Hafalan :* " +
+    recitation +
+    "%0a" +
+    "%0a" +
+    "PRESTASI PENDAFTAR" +
+    "%0a" +
+    "*Kelebihan Pada IT / DKV :* " +
+    achievement +
+    "%0a" +
+    "*Pencapaian / Prestasi / Keahlian Khusus :* " +
+    "%0a" +
+    superiority +
+    "%0a" +
+    "%0a" +
+    "*Hobby :* " +
+    "%0a" +
+    "%0a" +
+    "*WEB DAN SOSMED*" +
+    "%0a" +
+    "*Web Blog :* " +
+    blog +
+    "%0a" +
+    "*Pernah Upload Video Di YouTube? :* " +
+    video +
+    "%0a" +
+    "*Facebook :* " +
+    facebook +
+    "%0a" +
+    "*Instagram :* " +
+    instagram +
+    "%0a" +
+    "*No. Whatsapp :* " +
+    whatsapp +
+    "%0a" +
+    "%0a" +
+    "*TOKOH DAN ULAMA*" +
+    "%0a" +
+    "*Tokoh Teladan Dan Cara Menirunya :* " +
+    "%0a" +
+    example +
+    "%0a" +
+    "%0a" +
+    "*REKOMENDASI USTADZ / PESANTREN / YAYASAN*" +
+    "%0a" +
+    "*Darimana Tahu Ma'had IT? :* " +
+    recommendation +
+    "%0a" +
+    "*Motivasi Masuk Ma'had IT :* " +
+    motivation +
+    "%0a" +
+    "*Siap Ikuti Aturan? :* " +
+    readiness;
 
   window.open(url, "_blank").focus();
 }
